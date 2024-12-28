2474231d469a4cd0203bfa0d0000002c283d34ef
Erik Pohl <epohl@dmigs.com>
2024-12-28 12:48:11 -0500

    modified license changing function

--- foundation.py
+++ foundation.py
@@ -10,7 +10,7 @@ def safe_copy(src, dest):
     if os.path.exists(dest):
         logger.info(f"adding a .resource to the file name so as not to overwrite existing file with same name")
         dest += '.resource'
-    logger.info(f"copying {repo_resource_name.name} to {args.dest + repo_resource_name.name}")
+    logger.info(f"copying {repo_resource.name} to {args.dest + repo_resource.name}")
     shutil.copyfile(src, dest)
     return True
 
@@ -25,7 +25,7 @@ def append_authors(dest_repo, repo_folder):
             author_name = first_commit.name
             author_email = first_commit.email
             today = date.today()
-            formatted_date = today.strftime("%d/%m/%Y")
+            formatted_date = today.strftime("%m/%d/%Y")
             try:
                 authors_handle.write(f"{author_name}|{author_email}|{formatted_date}\n")
             except IOError:
@@ -36,6 +36,32 @@ def append_authors(dest_repo, repo_folder):
             return False
     return True
 
+def update_license(dest_repo, repo_folder):
+    logger.info("Modifying license to reflect current repo owner")
+    try:
+        first_commit = dest_repo.head.commit.author
+        author_name = first_commit.name
+    except:
+        logger.info("couldn't derive author name from destination repo, using _____")
+        author_name = '_______'
+    today = date.today()
+    formatted_date = today.strftime("%Y")
+    logger.info(f"Identified {author_name} for License and {formatted_date} for year")
+    lines = []
+    logger.info("reading existing license file")
+    with open(repo_folder + "LICENSE.", "rt") as license_handle:
+        for line in license_handle:
+            oldline = line
+            line = line.replace("Erik Pohl", author_name)
+            line = line.replace('2024', formatted_date)
+            if line != oldline:
+                logger.info("made a replacement")
+            lines.append(line)
+    logger.info("writing new license file")
+    with open(repo_folder+"LICENSE.", "wt") as license_handle:
+        for line in lines:
+            license_handle.write(line)
+    return True
 
 if __name__ == '__main__':
     logger.info("Attempting to build a foundation based on https://github.com/ErikPohl444/resources")
@@ -58,15 +84,18 @@ if __name__ == '__main__':
         logger.info(f"{args.dest} exists, so we can build the foundation")
     ignore_files = ["foundation.py", "README.md"]
     logger.info(f"defined ignore file list [{ignore_files}] for files not to copy to destination")
-    for repo_resource_name in dest_repo.tree().traverse():
-        dest_file_name = args.dest + repo_resource_name.name
-        logger.info(f"naming {dest_file_name} as the destination file and path")
-        if repo_resource_name.name == 'readme_template.md':
-            logger.info(f"converting readme template to readme: {repo_resource_name.name} to README.md")
+    for repo_resource in dest_repo.tree().traverse():
+        resource_name = repo_resource.name
+        dest_file_name = args.dest + resource_name
+        logger.info(f"naming {dest_file_name} as the destination file and path for {resource_name}")
+        if resource_name == 'readme_template.md':
+            logger.info(f"converting readme template to readme: {resource_name} to README.md")
             safe_copy('readme_template.md', args.dest + 'README.md')
-        elif repo_resource_name.name in ignore_files:
-            logger.info(f"ignoring {repo_resource_name.name} from copy to destination")
+        elif resource_name in ignore_files:
+            logger.info(f"ignoring {resource_name} from copy to destination")
         else:
-            safe_copy(repo_resource_name.name, dest_file_name)
-            if repo_resource_name.name == "AUTHORS.md":
+            safe_copy(resource_name, dest_file_name)
+            if resource_name == "AUTHORS.md":
                 append_authors(dest_repo, args.dest)
+            if resource_name == "LICENSE":
+                update_license(dest_repo, args.dest)
