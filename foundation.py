6f0ceaa1670c6c4fc4b9da2211a6072dd1d0248f
Erik Pohl <epohl@dmigs.com>
2024-12-28 11:44:00 -0500

    cleaned code using functions

--- foundation.py
+++ foundation.py
@@ -2,6 +2,33 @@ import os
 import shutil
 import argparse
 import git
+from datetime import date
+
+
+def safe_copy(src, dest):
+    if os.path.exists(dest):
+        print(f"adding a .resource to the file name so as not to overwrite existing file with same name")
+        dest += '.resource'
+    print(f"copying {item.name} to {args.dest + item.name}")
+    shutil.copyfile(src, dest)
+    return True
+
+
+def append_authors(dest, repo_folder):
+    with open(dest, "wt") as authors_handle:
+        authors_handle.write('Author name|Author Email|Entry date\n')
+        authors_handle.write('-----------|------------|----------\n')
+        try:
+            dest_repo = git.Repo(repo_folder)
+            first_commit = dest_repo.head.commit.author
+            author_name = first_commit.name
+            author_email = first_commit.email
+            today = date.today()
+            formatted_date = today.strftime("%d/%m/%Y")
+            authors_handle.write(f"{author_name}|{author_email}|{formatted_date}\n")
+        except ValueError:
+            print("couldn't access author info for destination repo because there is no commit history")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="move foundation files")
@@ -17,32 +44,14 @@ if __name__ == '__main__':
     ignore_files = ["foundation.py", "README.md"]
 
     for item in x.tree().traverse():
-        print(item.name, args.dest)
+        dest_file_name = args.dest + item.name
+        print(f"naming {dest_file_name} as the destination file and path")
         if item.name == 'readme_template.md':
             print(f"converting readme template to readme: {item.name} to README.md")
-            shutil.copyfile('readme_template.md', args.dest + 'README.md')
+            safe_copy('readme_template.md', args.dest + 'README.md')
         elif item.name in ignore_files:
-            print(f"ignore {item.name}")
+            print(f"ignoring {item.name}")
         else:
-            if os.path.exists(args.dest+item.name):
-                print(f"adding a .donotuse to the file name so as not to overwrite existing file with same name")
-                print(f"copying {item.name} to {args.dest + item.name}")
-                shutil.copyfile(item.name, args.dest+item.name+".donotuse")
-            else:
-                print(f"copying {item.name} to {args.dest + item.name}")
-                shutil.copyfile(item.name, args.dest+item.name)
-                if item.name == "AUTHORS.md":
-                    with open(args.dest+item.name, "wt") as authors_handle:
-                        authors_handle.write('Author name|Author Email|Entry date\n')
-                        authors_handle.write('-----------|------------|----------\n')
-                        try:
-                            dest_repo = git.Repo(args.dest)
-                            first_commit = dest_repo.head.commit.author
-                            author_name = first_commit.name
-                            author_email = first_commit.email
-                            authors_handle.write(f"{author_name}|{author_email}|12/27/24\n")
-                        except:
-                            print("couldn't access author info for destination repo because there is no commit history")
-                
-
-
+            safe_copy(item.name, dest_file_name)
+            if item.name == "AUTHORS.md":
+                append_authors(dest_file_name, args.dest)
